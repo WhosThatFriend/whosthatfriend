94d307545b0ba2251dd9885eb4a8a8722562cc8e
Jon Lao <hyperation@hotmail.com>
2011-12-01 20:49:27 -1000

    Added Facebook login

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,15 @@
 package controllers;
 
 import play.*;
+import play.modules.facebook.FbGraph;
+import play.modules.facebook.FbGraphException;
+import play.modules.facebook.Parameter;
 import play.mvc.*;
+import play.mvc.Scope.Session;
 
 import java.util.*;
+
+import com.google.gson.JsonObject;
 
 import models.*;
 
@@ -11,6 +17,25 @@ public class Application extends Controller {
 
     public static void index() {
         render();
+    }
+
+    public static void facebookLogin() {
+        try {
+            JsonObject profile = FbGraph.getObject("me"); // fetch the logged in user
+            String email = profile.get("email").getAsString(); // retrieve the email
+            // do useful things
+            Session.current().put("username", email); // put the email into the session (for the Secure module)
+        } catch (FbGraphException fbge) {
+            flash.error(fbge.getMessage());
+            if (fbge.getType() != null && fbge.getType().equals("OAuthException")) {
+                Session.current().remove("username");
+            }
+        }
+        redirect("/");
+    }
+
+    public static void logout() {
+    	render();
     }
 
 }
